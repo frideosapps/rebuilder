e969c49c93915396a10755f61b06d045f89e8403
Frideos <frideosapps@gmail.com>
2019-06-01 17:28:47 +0200

    example updated

--- example/lib/src/mainpage.dart
+++ example/lib/src/mainpage.dart
@@ -17,8 +17,6 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   RebuilderObject<int> bottomTab;
 
-  void _changeBottomTab(int index) => bottomTab.value = index;
-
   Future isUsersListLoaded;
 
   @override
@@ -28,23 +26,6 @@ class _MainPageState extends State<MainPage> {
     bottomTab = RebuilderObject<int>.init(
         rebuilderState: appModel.homePage, initialData: 0);
     isUsersListLoaded = appModel.repository.getUsers();
-  }
-
-  Widget _switchPage(AppModel appModel) {
-    switch (bottomTab.value) {
-      case 0:
-        return DataModelProvider<CountersModel>(
-          dataModel: appModel.countersModel,
-          child: const CountersPage(),
-        );
-      case 1:
-        return DataModelProvider<TreeModel>(
-          dataModel: appModel.treeModel,
-          child: const TreePage(),
-        );
-      case 2:
-        return const SettingsPage();
-    }
   }
 
   @override
@@ -63,27 +44,75 @@ class _MainPageState extends State<MainPage> {
                   ),
                   body: !snapshot.hasData
                       ? Center(child: const CircularProgressIndicator())
-                      : _switchPage(appModel),
-                  bottomNavigationBar: BottomNavigationBar(
-                    onTap: _changeBottomTab,
-                    currentIndex: bottomTab.value,
-                    items: [
-                      const BottomNavigationBarItem(
-                        icon: Icon(Icons.computer),
-                        title: Text('Counters'),
-                      ),
-                      const BottomNavigationBarItem(
-                        icon: Icon(Icons.filter_list),
-                        title: Text('Tree'),
-                      ),
-                      const BottomNavigationBarItem(
-                        icon: Icon(Icons.settings),
-                        title: Text('Settings'),
-                      ),
-                    ],
+                      : SwitchTabWidget(
+                          appModel: appModel,
+                          bottomTab: bottomTab,
+                        ),
+                  bottomNavigationBar: BottomNavigationBarWidget(
+                    bottomTab: bottomTab,
                   ),
                 );
               });
         });
+  }
+}
+
+class SwitchTabWidget extends StatelessWidget {
+  const SwitchTabWidget(
+      {Key key, @required this.appModel, @required this.bottomTab})
+      : super(key: key);
+
+  final AppModel appModel;
+  final RebuilderObject<int> bottomTab;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (bottomTab.value) {
+      case 0:
+        return DataModelProvider<CountersModel>(
+          dataModel: appModel.countersModel,
+          child: const CountersPage(),
+        );
+      case 1:
+        return DataModelProvider<TreeModel>(
+          dataModel: appModel.treeModel,
+          child: const TreePage(),
+        );
+      case 2:
+        return const SettingsPage();
+      default:
+        return Container();
+    }
+  }
+}
+
+class BottomNavigationBarWidget extends StatelessWidget {
+  const BottomNavigationBarWidget({Key key, @required this.bottomTab})
+      : super(key: key);
+
+  final RebuilderObject<int> bottomTab;
+
+  void _changeBottomTab(int index) => bottomTab.value = index;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: _changeBottomTab,
+      currentIndex: bottomTab.value,
+      items: [
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.computer),
+          title: Text('Counters'),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.filter_list),
+          title: Text('Tree'),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          title: Text('Settings'),
+        ),
+      ],
+    );
   }
 }
